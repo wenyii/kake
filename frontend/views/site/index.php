f823cb916515f2d49645da0f2c0f4cd23bcedfa7
yiwenjianglemon <jiangwenyilemon@163.com>
2017-06-13 14:07:14 +0800

    del_company_tel

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -52,7 +52,7 @@ $params = \Yii::$app->params;
             <span>
                 <img src="<?= $params['frontend_source'] ?>/img/classify.svg"/>
             </span>
-            闪购专区
+            喀客专线
             <a href="<?= Url::to(['items/index']) ?>">
                 <div class="experience-1-more">更多
                     <img class="img-responsive"
