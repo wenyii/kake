a955a6a6ea0233f4d19934becca79d3778920b0f
yiwenjianglemon <jiangwenyilemon@163.com>
2017-08-04 11:02:57 +0800

    新版本页面

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -8,21 +8,21 @@ $params = \Yii::$app->params;
 ?>
 
 <div class="body">
+    <!-- Banner -->
     <div class="banner" kk-fixed>
-        	<div class="menu-box"  kk-menu="#menu" data-pos-x="-15" data-pos-y="-15">
-	    		<div class="menu">
-	            <img src="<?= $params['frontend_source'] ?>/img/menu.svg"/>
-	        </div>
-	    	</div>
+        <div class="menu-box" kk-menu="#menu" data-pos-x="-15" data-pos-y="-15">
+            <div class="menu">
+                <img src="<?= $params['frontend_source'] ?>/img/menu.svg"/>
+            </div>
+        </div>
         <?php if (!empty($focusList)): ?>
-	        <ul class="focus-point">
-	        		<?php foreach ($focusList as $focus): ?>
-	        			<li></li>
-	        		<?php endforeach ?>
-	        	</ul>
-        	<?php endif ?>
+            <ul class="focus-point">
+                <?php foreach ($focusList as $focus): ?>
+                    <li></li>
+                <?php endforeach ?>
+            </ul>
+        <?php endif ?>
     </div>
-
     <?php if (!empty($focusList)): ?>
         <div class="carousel" id="focus-hot" kk-focus=".focus-point" data-point-current="on" style="overflow:hidden">
             <div class="carousel-scroller product-focus">
@@ -45,53 +45,93 @@ $params = \Yii::$app->params;
         </div>
 
     <?php endif; ?>
-</div>
-<?php if (!empty($flashSalesList)): ?>
-    <div class=" experience">
-        <div class=" experience-1">
-            <span>
-                <img src="<?= $params['frontend_source'] ?>/img/classify.svg"/>
-            </span>
-            喀客专线
-            <a href="<?= Url::to(['items/index']) ?>">
-                <div class="experience-1-more">更多
-                    <img class="img-responsive" src="<?= $params['frontend_source'] ?>/img/more.svg"/>
-                </div>
+
+    <!-- Hot-aim -->
+    <div class="kake-box hot-aim">
+        <div class="kake-title">
+            <h3>
+                <img src="<?= $params['frontend_source'] ?>/img/index-icon-aim.svg"/>
+                热门目的地
+            </h3>
+            <a href="<?= Url::to(['items/region']) ?>">
+                更多
+                <img src="<?= $params['frontend_source'] ?>/img/index-icon-more.svg"/>
             </a>
         </div>
-        <div class="carousel" id="carousel-scroller" kk-scroll>
+        <div class="carousel kake-theme" id="carousel-scroller-aim" kk-scroll>
             <div class="carousel-scroller scroll">
-                <?php foreach ($flashSalesList as $flashSales): ?>
-                    <div>
+                <?php foreach ($plateList as $i => $item): ?>
+                    <?php $cls = ($i % 2 == 0) ? null : 'class="top20"' ?>
+                    <div <?= $cls ?>>
                         <a href="<?= Url::to([
-                            'detail/index',
-                            'id' => $flashSales['id']
-                        ]) ?>" >
-                            <img class="img-responsive" src="<?= current($flashSales['cover_preview_url']) ?>"/>
+                            'items/index',
+                            'plate' => $item['id']
+                        ]) ?>">
+                            <img src="<?= current($item['preview_url']) ?>"/>
                         </a>
-                        <p><?= $flashSales['title'] ?></p>
                     </div>
                 <?php endforeach ?>
             </div>
         </div>
     </div>
-<?php endif; ?>
 
-<?php if (!empty($banner)): ?>
-    <div class="carousel-scroll" id="carousel-scroller-activity" kk-scroll>
-        <div class="carousel-scroller activity">
-            <?php foreach ($banner as $item): ?>
-                <a href="<?= $item['url'] ?>" target="<?= $item['target_info'] ?>">
-                    <img class="img-responsive" src="<?= current($item['preview_url']) ?>"/>
-                </a>
-            <?php endforeach; ?>
+    <!-- Flash-shopping-zone -->
+    <div class="kake-box flash-shopping-zone">
+        <div class="kake-title">
+            <h3>
+                <img src="<?= $params['frontend_source'] ?>/img/index-icon-sales.svg"/>
+                闪购专区
+            </h3>
+            <a href="<?= Url::to(['items/index']) ?>">
+                更多
+                <img src="<?= $params['frontend_source'] ?>/img/index-icon-more.svg"/>
+            </a>
+        </div>
+        <div class="carousel kake-theme" id="carousel-scroller-flash" kk-camel>
+            <div class="carousel-scroller scroll">
+                <div class="product_image"></div>
+                <?php foreach ($flashSalesList as $flashSales): ?>
+                    <div class="product_image">
+                        <a href="<?= Url::to([
+                            'detail/index',
+                            'id' => $flashSales['id']
+                        ]) ?>">
+                            <img class="img-responsive" src="<?= current($flashSales['cover_preview_url']) ?>"/>
+                        </a>
+                        <p><?= $flashSales['title'] ?></p>
+                    </div>
+                <?php endforeach ?>
+                <div class="product_image"></div>
+            </div>
         </div>
     </div>
-<?php endif; ?>
 
-<div class="recommend">
-    <?php if (!empty($standardHtml)): ?>
-        <p><span class="recommend2">精品推荐</span></p>
-        <?= $standardHtml ?>
-    <?php endif; ?>
+    <!-- Activity -->
+    <div class="kake-box activity">
+        <?php if (!empty($banner)): ?>
+            <div class="carousel-scroll" id="carousel-scroller-activity" kk-scroll>
+                <div class="carousel-scroller activity">
+                    <?php foreach ($banner as $item): ?>
+                        <a href="<?= $item['url'] ?>" target="<?= $item['target_info'] ?>">
+                            <img class="img-responsive" src="<?= current($item['preview_url']) ?>"/>
+                        </a>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        <?php endif; ?>
+    </div>
+
+    <!-- Recommend-item -->
+    <div class="kake-box product">
+        <div class="title">
+            <img src="<?= $params['frontend_source'] ?>/img/index-icon-recommand.png"/>
+        </div>
+        <div class="list">
+            <ul>
+                <?php if (!empty($standardHtml)): ?>
+                    <?= $standardHtml ?>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
 </div>
