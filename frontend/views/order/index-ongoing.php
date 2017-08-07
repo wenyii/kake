705a0eb14e68c7375598bad8998a269cfb20569f
yiwenjianglemon <jiangwenyilemon@163.com>
2017-08-07 18:02:13 +0800

    修1.列表页右上角icon更换 2.首页活动banner

--- frontend/views/order/index-ongoing.php
+++ frontend/views/order/index-ongoing.php
@@ -10,7 +10,7 @@ $params = \Yii::$app->params;
 <header>
     订单中心
     <div class="menu detail" kk-menu="#menu">
-        <img class="img-responsive" src="<?= $params['frontend_source'] ?>/img/menu1.svg"/>
+        <img class="img-responsive" src="<?= $params['frontend_source'] ?>/img/list.svg"/>
     </div>
 </header>
 <div class="classify">
